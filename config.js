c388343337ada4e7859d4b1c8d68490dce1f36cf
MarxVn09 <phamvanhoang1092004@gmail.com>
2025-01-20 21:25:06 +0700

    all
    add all

---
+++ config.js
@@ -0,0 +1,15 @@
+const CONFIG = {
+    introTitle: 'Hi luuu!',
+    introDesc: `Xin lõi vì đã trêu ấy :(((`,
+    btnIntro: 'huhu',
+    title: 'Đánh giá mức độ giận tại đây 🥲',
+    btnYes: 'Giận thiệt',
+    btnNo: 'Không, bth  :3',
+    question:'Hãy tha lũi cho tui nhoooo',
+    btnReply: 'Gửi cho Marx <3',
+    reply: 'ukiiiiii ',
+    mess: 'Tui biết mà 🥰.',
+    messDesc: 'Xin lũi vì đã làm ấy giận nhoooo.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/marxnosuy'
+}
